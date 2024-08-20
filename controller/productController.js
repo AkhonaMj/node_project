bccf72550ca7fbdf4ad3012ec4111505fb710419
AkhonaMj <akhona0304mjikelo@gmail.com>
2024-08-20 13:43:42 +0200

    product add route

--- controller/productController.js
+++ controller/productController.js
@@ -1,4 +1,4 @@
-import {getProductsDB, getProductByIDDB} from "../model/productDB.js"
+import {getProductsDB, getProductByIDDB, addProductDB} from "../model/productDB.js"
 
 const getProducts = async (req, res)=>{
     res.send(await getProductsDB())
@@ -8,6 +8,13 @@ const getProductByID = async (req, res)=>{
     res.json(await getProductByIDDB(req.params.id))
 }
 
+const addProduct = async (req, res) => {
+    let {prodName, quantity, amount, Category, prodUrl} = req.body
+    await addProductDB(prodName, quantity, amount, Category, prodUrl)
+
+    res.send("Product Added!")
+    
+}
 
 
-export {getProducts, getProductByID}
+export {getProducts, getProductByID, addProduct}
--- model/productDB.js
+++ model/productDB.js
@@ -9,4 +9,9 @@ const getProductByIDDB = async (prodID) => {
     let [results] = await pool.query("SELECT * FROM bntypjaquqwm92oofyxl.products WHERE prodID =?", [prodID])
     return results
 }
-export {getProductsDB, getProductByIDDB}
+
+const addProductDB = async (prodName, quantity, amount, Category, prodUrl) => {
+    await pool.query("INSERT INTO  bntypjaquqwm92oofyxl.products(prodName, quantity, amount, Category, prodUrl) VALUES(?,?,?,?,?)", [prodName, quantity, amount, Category, prodUrl])
+    
+}
+export {getProductsDB, getProductByIDDB, addProductDB}
