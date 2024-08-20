0daaaf5584bbe7a68c1cda76da5fd71397056fa1
RichtersHotline <KeenanTimmieWeb@gmail.com>
2024-08-20 09:57:57 +0200

    initial styling

--- src/views/HomePageView.vue
+++ src/views/HomePageView.vue
@@ -1,5 +1,18 @@
 <template>
-  
+    <div class="container-fluid BG">
+        <div class="row align-items-center respon">
+          <div class="col">
+   
+            </div>
+                
+                 
+              </div>
+          
+          <div class="col">
+            
+          </div>
+        </div>
+    
 </template>
 
 <script>
