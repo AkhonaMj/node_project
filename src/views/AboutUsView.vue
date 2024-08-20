630f73fa16d1a10dddb44d24a81a2642f4d6c33e
RichtersHotline <KeenanTimmieWeb@gmail.com>
2024-08-20 16:15:16 +0200

    About Page Setup

---
+++ src/views/AboutUsView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+        <h2 class="display-2">About</h2>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
