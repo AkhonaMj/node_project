f19d326d5144fe93e31995a21022c7f27acff012
RichtersHotline <KeenanTimmieWeb@gmail.com>
2024-08-20 10:59:01 +0200

    Added a Landing Page

--- src/views/HomePageView.vue
+++ src/views/HomePageView.vue
@@ -1,16 +1,13 @@
 <template>
     <div class="container-fluid BG">
-        <div class="row align-items-center respon">
-          <div class="col">
-   
-            </div>
-                
-                 
-              </div>
-          
-          <div class="col">
-            
-          </div>
+        <div class="LandingPage">
+            <div class="Headline">
+            <h2 class="display-2">Personified</h2>
+            <h1 class="display-1">Elegance</h1>
+            <button type="button" id="HeadBtn">See Our Collection</button>
+        </div>
+        <img src="https://richtershotline.github.io/Pimages/Images/LandingImage.jpg" class="LandingPhoto" alt="LandingPhoto" loading="lazy">
+    </div>
         </div>
     
 </template>
