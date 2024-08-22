3c08d724ec05dac954d971b1344f82f365bcaff8
AkhonaMj <akhona0304mjikelo@gmail.com>
2024-08-22 12:33:07 +0200

    added error handling

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,28 +1,44 @@
-import {compare} from "bcrypt"
-import { getUserByEmailDB } from "../model/userDB.js"
-import jwt from "jsonwebtoken" 
-import {config} from "dotenv"
-config()
+import { compare } from "bcrypt";
+import { getUserByEmailDB } from "../model/userDB.js";
+import jwt from "jsonwebtoken";
+import { config } from "dotenv";
+config();
 
 const checkUser = async (req, res, next) => {
-    const {emailAdd, userPass} = req.body //destructuring
+  try {
+    const { emailAdd, userPass } = req.body; //destructuring
     console.log(emailAdd, userPass);
-    
-    let hashedPassword = (await getUserByEmailDB(emailAdd))
-console.log(hashedPassword);
 
-    compare(userPass, hashedPassword, (err, result)=>{
-        if(result==true){
-            let token = jwt.sign({emailAdd:emailAdd},process.env.SECRET_KEY,{expiresIn: "1h"})
-            console.log(token)
-            req.body.token = token
+    let hashedPassword = await getUserByEmailDB(emailAdd);
+    console.log(hashedPassword);
 
-            next()
-            return
-        }
-        res.send("Wrong password")
-    })
- 
-}
+    if (!hashedPassword) {
+      res.status(404).send("User not found");
+      return;
+    }
 
-export {checkUser}
+    compare(userPass, hashedPassword, (err, result) => {
+      if (err) {
+        res.status(500).send("Internal Server Error");
+        return;
+      }
+
+      if (result == true) {
+        let token = jwt.sign(
+          { emailAdd: emailAdd },process.env.SECRET_KEY, { expiresIn: "1h" }
+        );
+        console.log(token);
+        req.body.token = token;
+
+        next();
+        return;
+      }
+      res.status(401).send("Wrong password");
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+};
+
+export { checkUser };
