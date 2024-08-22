02f2ae9625a8e2f8b46cae2eae5684605be19d20
RichtersHotline <KeenanTimmieWeb@gmail.com>
2024-08-22 21:08:25 +0200

    added price searching and category searching

--- src/views/ProductPageView.vue
+++ src/views/ProductPageView.vue
@@ -9,8 +9,10 @@
             </div>
         </div>
         <div class="SortButtons mt-4">
-            <button type="button" id="SortBtnPrice" @click="Sort">Sort By Highest</button>
-            <button type="button" id="SortBtn">Sort By Category</button>
+            <button type="button" id="SortBtnPrice" ref="Sorter" @click="Sort()">Sort By Highest</button>
+            <button type="button" id="SortBtn" @click="SortCategoryW()">Sort By Women</button>
+            <button type="button" id="SortBtn" @click="SortCategoryM()">Sort By Men</button>
+
         </div>
         <div class="Products mt-3 mx-auto" v-if="SearchedProducts">
             <CardComp v-for="product in SearchedProducts" :key="product.prodID">
@@ -40,7 +42,7 @@ export default {
     data() {
     return {
       SearchData: "", 
-     
+      sorterOn: true
     };
   },
 components: {
@@ -60,7 +62,8 @@ computed: {
 
 
    )
-}
+},
+
 
 },
 props: ["searchBar"],   
@@ -69,13 +72,48 @@ props: ["searchBar"],
     
 methods: {
 
-    Sort() {
 
 
-        console.log("You pressed it congrats!")
+  Sort() {
+    try {
+        if (this.sorterOn) {
+    this.products.sort((cheap, pricey) => pricey.amount - cheap.amount);
+     this.sorterOn = false;
+      this.$refs.Sorter.innerHTML = "Sort By Highest";
+     } else {
+       this.products.sort((cheap, pricey) => cheap.amount - pricey.amount);
+       this.sorterOn = true;
+       this.$refs.Sorter.innerHTML = "Sort By Lowest";
+        }
+    } catch (e) {
+        throw new Error("Cannot sort at this time");
+    }
+},
+SortCategoryW() {
+    try {
+        console.log("Women clicked");
 
-    },
+        // this filters by women
+        const WomenProducts = this.products.filter(unit => unit.Category.includes("women"));
+        this.$store.state.products = WomenProducts; // Update the products array
 
+    } catch (e) {
+        throw new Error("Cannot sort at this time. Please contact the owner.");
+    }
+},
+SortCategoryM() {
+    try {
+        console.log("Men clicked");
+
+        // Filter products for men
+        const MenProducts = this.products.filter(unit => unit.Category.includes("men"));
+        console.log("MenProducts:", MenProducts); // Log the filtered products
+        this.$store.state.products = MenProducts; // Update the products array
+
+    } catch (e) {
+        throw new Error("Cannot sort at this time. Please contact the owner.");
+    }
+},
 
     
     SearchItem() {
