27f338bff0f47804b9a48b49b532bcbe8322dce0
RichtersHotline <KeenanTimmieWeb@gmail.com>
2024-08-22 21:20:43 +0200

    footer added

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,17 @@
 <main class="container-fluid">
   <router-view/>
 </main>
+<FooterComp/>
 </div>
 </template>
 <script>
 import NavBarComp from "@/components/NavBarComp.vue";
-
+import FooterComp from "@/components/FooterComp.vue";
 export default {
 components: {
 
-  NavBarComp
+  NavBarComp,
+  FooterComp
 }
 
 }
---
+++ src/components/FooterComp.vue
@@ -0,0 +1,34 @@
+<template>
+    <footer class="container-fluid footerstyle">
+  <div class="row">
+  <p class="text-center">Valentine's &copy; <span>{{cYear}}</span>
+  
+  </p>
+  
+  </div>
+  
+    </footer>
+  
+  </template>
+  
+  <script>
+  export default {
+  data() {
+  
+      return {
+    cYear: new Date().getUTCFullYear()
+  
+  
+      }
+  
+  
+  
+  },
+  
+  
+  }
+  </script>
+  
+  <style>
+  
+  </style>
