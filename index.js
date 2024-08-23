1281fbeacad71ef5bbd65a9c85eba1f7c611889b
AkhonaMj <akhona0304mjikelo@gmail.com>
2024-08-23 16:41:35 +0200

    updated index.js

--- index.js
+++ index.js
@@ -7,11 +7,7 @@ let port = process.env.PORT || 2020
 const app = express()
 app.use(express.static('public'))
 app.use(express.json())
-app.use(cors({
-    origin: '*',
-    credentials:true
-  }))
-  app.use((req, res, next) => {
+app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Methods", "*");
@@ -19,7 +15,8 @@ app.use(cors({
     res.header("Access-Control-Allow-Headers", "*");
     res.header("Access-Control-Expose-Headers", "Authorization");
     next();
-  });
+});
+app.use(cors())
   
 app.use("/users", userRouter)
 app.use("/products", productRouter )
